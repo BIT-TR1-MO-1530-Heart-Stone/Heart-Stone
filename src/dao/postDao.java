3c4c9475c925c92f6cf93f8708aef6d0046691f7
Xinyu Chen <s3798356@student.rmit.edu.au>
2021-08-28 13:25:23 +0800

    postDao

--- src/dao/postDao.java
+++ src/dao/postDao.java
@@ -39,6 +39,45 @@ public class postDao {
 //        prst = conn.prepareStatement(deletePost); // ��sql������Ԥ����
 //    }
 
-
-
+//    public static List<Post> getAllPosts() throws Exception{
+//         ArrayList<Post> postList = new ArrayList<Post>();
+//         try {
+//             Statement statement = conn.createStatement();
+//             statement.setQueryTimeout(30);
+//             String query = " SELECT U.USER_SCREENNAME,\n" + "           U.USER_PROFILE_PICTURE,\n"
+//                     + "           P.POST_ID,\n" + "           P.USER_ID,\n" + "           P.POST_CONTENT,\n"
+//                     + "           P.ORIGINAL,\n" + "           P.REPOST_ID,\n"
+//                     + "           TO_CHAR(P.POST_DATE, 'yyyy-MM-dd hh24:mi:ss') POST_DATE\n"
+//                     + "      FROM post P, user U\n" + "     WHERE P.USER_ID = U.USER_ID\n"
+//                     + "     ORDER BY P.POST_DATE";
+//             ResultSet results = statement.executeQuery(query);
+//             Post post = null;
+//             User user = null;
+//             while (results.next()) {
+//                 post = new Post();
+//                 user = new User();
+//                 String userId = results.getString("user_id");
+//                 String postId = results.getString("post_id");
+//                 // Like 0——no 1——yes
+//                 int isLike = isLike(userId, postId);
+//                 post.setLikes(isLike);
+//                 user.setScreenname(results.getString("user_screenname"));
+//                 user.setProfile_picture(results.getString("user_profile_picture"));
+//                 post.setUser(user);
+//                 post.setPostId(postId);
+//                 post.setUserId(userId);
+//                 post.setContent(results.getString("post_content"));
+//                 post.setPostDate(results.getString("post_date"));
+//                 post.setOriginal(Integer.valueOf(results.getString("original")));
+//                 post.setRepostId(Integer.valueOf(results.getString("repost_id")));
+//                 post.setCommentCount(queryCommentCount(postId));
+//                 post.setLikeCount(queryLikeCount(postId));
+//                 postList.add(post);
+//             }
+//             statement.close();
+//         } catch (SQLException e) {
+//             System.err.println(e.getMessage());
+//         }
+//         return postList;
+//     }
 }
