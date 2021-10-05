3d4ec89a569675c3efa1643cb8890442323b78b1
Xinyu Chen <s3798356@student.rmit.edu.au>
2021-10-05 13:14:57 +0800

    like backend fix

--- src/main/java/dao/LikeDao.java
+++ src/main/java/dao/LikeDao.java
@@ -11,30 +11,28 @@ public class LikeDao {
 
     public static boolean like(LikePost likePost) throws Exception {
         try {
-            conn = JDBCutil.getCon();
-            Statement statement = conn.createStatement();
-            statement.setQueryTimeout(30);
-            String sss = "insert into likePost (Like_Time,User_id,Post_id) values(?,?,?)";
-            prst = conn.prepareStatement(sss);
-            prst.setString(1, likePost.getLike_date());
-            prst.setInt(2, likePost.getUser_id());
-            prst.setInt(3, likePost.getPost_id());
-            int result = statement.executeUpdate(sss);
-            statement.close();
-            return result > 0 ? true : false;
+        conn = JDBCutil.getCon();
+        Statement statement = conn.createStatement();
+        statement.setQueryTimeout(30);
+        String sss = " INSERT INTO T_like (Like_Time,User_id,Post_id) VALUES (?,?,?)";
+        prst = conn.prepareStatement(sss);
+        prst.setString(1, likePost.getLike_date());
+        prst.setInt(2, likePost.getUser_id());
+        prst.setInt(3, likePost.getPost_id());
+        boolean result = prst.executeUpdate() > 0;
+        return result;
         } catch (SQLException e) {
             System.err.println(e.getMessage());
             return false;
         }
     }
 
-
     public static boolean unlike(LikePost likePost) throws Exception {
         try {
             conn = JDBCutil.getCon();
             Statement statement = conn.createStatement();
             statement.setQueryTimeout(30);
-            String unlike = "DELETE FROM likePost WHERE User_id=? and Post_id=?";
+            String unlike = "DELETE FROM T_like T WHERE T.User_id=? and T.Post_id=?";
             prst = conn.prepareStatement(unlike);
             prst.setInt(1, likePost.getUser_id());
             prst.setInt(2, likePost.getPost_id());
@@ -45,47 +43,39 @@ public class LikeDao {
             System.err.println(e.getMessage());
             return false;
         }
-    }
+    } /* Whether the user likes this post or not 0 - No 1 - yes*/
 
-    // Whether the user likes this post or not 0 - No 1 - yes
-    public static int isLike(int userId, String postId) throws Exception{
+    public static int isLike(int userId, String postId) throws Exception {
         int res = 0;
         try {
             conn = JDBCutil.getCon();
             Statement statement = conn.createStatement();
             statement.setQueryTimeout(30);
-            String query = "SELECT T.ID  FROM likePost T WHERE T.User_id = '" + userId + "'  AND T.Post_id = '"
-                    + postId + "'";
+            String query = "SELECT T.ID  FROM T_like T WHERE T.User_id = '" + userId + "'  AND T.Post_id = '" + postId + "'";
             System.out.println(query);
             ResultSet results = statement.executeQuery(query);
-            if (results.next()) {
-                res = 1;
-            }
+            if (results.next()) res = 1;
+            statement.close();
+        } catch (SQLException e) {
+            return res;
+        }
+        return res;
+    } /* Query the number of likes of microblog*/
+
+    public static int queryLikeCount(int postId) throws Exception {
+        int res = 0;
+        try {
+            conn = JDBCutil.getCon();
+            Statement statement = conn.createStatement();
+            statement.setQueryTimeout(30);
+            String query = "SELECT COUNT(*) LIKE_COUNT FROM T_like WHERE Post_id = '" + postId + "'";
+            System.out.println(query);
+            ResultSet results = statement.executeQuery(query);
+            while (results.next()) res = Integer.parseInt(results.getString("like_count"));
             statement.close();
         } catch (SQLException e) {
             return res;
         }
         return res;
     }
-
-    // Query the number of likes of microblog
-    public static int queryLikeCount(int postId) throws Exception{
-        int res = 0;
-        try {
-            conn = JDBCutil.getCon();
-            Statement statement = conn.createStatement();
-            statement.setQueryTimeout(30);
-            String query = "SELECT COUNT(*) LIKE_COUNT FROM likePost WHERE Post_id = '" + postId + "'";
-            System.out.println(query);
-            ResultSet results = statement.executeQuery(query);
-            while (results.next()) {
-                res = Integer.parseInt(results.getString("like_count"));
-            }
-            statement.close();
-        } catch (SQLException e) {
-            return res;
-        }
-        return res;
-    }
-
 }
